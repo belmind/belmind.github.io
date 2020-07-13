a18d836d0c18fdabe8773395cf27a742a1a1a72b
Belmin Dervisevic <belmin.dervisevic@hotmail.com>
2020-07-13 20:45:36 +0200

    Add `gtag.js`

---
+++ gtag.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139128408-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-139128408-2');
+  </script>
