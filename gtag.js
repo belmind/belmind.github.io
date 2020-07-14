10a6f27ec8926c64c187b304a9e9b4582d70103a
Belmin Dervisevic <belmin.dervisevic@hotmail.com>
2020-07-14 13:44:18 +0200

    Integrate Google Analytics into `index.html`

--- gtag.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-139128408-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-139128408-2');
-  </script>
